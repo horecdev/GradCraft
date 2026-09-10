2242f33f812986fcd9bc918f2eb7b6dccb54c1ed
ozehownik <horecki.michal@gmail.com>
2026-09-10 12:08:05 +0200

    Wrapped up GPT inference

--- gradc/nn/generators/gpt_generator.hpp
+++ gradc/nn/generators/gpt_generator.hpp
@@ -9,7 +9,7 @@ namespace gradc {
     template <typename T>
     struct GPTGenerator {
         public:
-            static std::string run_inference(GPT<T>& gpt, BytePairEncoding& bpe, int64_t context_length, const std::string& start_text, int64_t max_tokens, int64_t num_sequences, Device cpu, Device infer_device, std::optional<float> temperature) {
+            static std::vector<std::string> run_inference(GPT<T>& gpt, BytePairEncoding& bpe, int64_t context_length, const std::string& start_text, int64_t max_tokens, int64_t num_sequences, Device cpu, Device infer_device, std::optional<float> temperature) {
                 std::vector<uint32_t> tokens = bpe.encode(start_text);
                 int64_t current_length = std::ssize(tokens);
 
@@ -90,9 +90,30 @@ namespace gradc {
                     }
                 }
 
-                for (int64_t b = 0; b < num_sequences; ++b) {
+                Tensor<int64_t> final_cpu_tokens = moved_tokens.to(cpu);
+                final_cpu_tokens.realize();
+                int64_t* final_ptr = final_cpu_tokens._get_storage()->data();
 
+                std::vector<std::string> decoded_sequences;
+                decoded_sequences.reserve(num_sequences);
+
+                for (int64_t b = 0; b < num_sequences; ++b) {
+                    std::vector<uint32_t> current_seq;
+                    current_seq.reserve(max_tokens);
+
+                    for (int64_t t = 0; t < max_tokens; ++t) {
+                        int64_t token_id = final_ptr[b * max_tokens + t];
+                        
+                        if (token_id == 259) { // <|im_end|>
+                            break;
+                        }
+                        
+                        current_seq.push_back(static_cast<uint32_t>(token_id));
+                    }
+                    decoded_sequences.push_back(bpe.decode(current_seq));
                 }
+
+                return decoded_sequences;
             }
-    };
+        };     
 }
