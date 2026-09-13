0290102fa79d5da12d19ae9242c1f7fcee7f73d9
Michał Horecki <horecki.michal@gmail.com>
2026-09-13 20:07:11 +0200

    Change batch size and update file paths
    Updated batch size and file paths for data and models.

--- train.cpp
+++ train.cpp
@@ -13,7 +13,7 @@ int main() {
 
         // HYPERPARAMS
         int64_t B_target = 512; // 85 * 6 = 510
-        int64_t B_real = 1; // target 6 for 3090
+        int64_t B_real = 4;
         int64_t seq_len = 1024;
         int64_t vocab_size = 32768;
         int64_t embed_dim = 768;
@@ -36,7 +36,7 @@ int main() {
         int64_t warmup_steps = 400; // ~5%
 
         // DATA
-        DataLoader loader = DataLoader("C:/Local Projects/autograd_cpp/data/datasets/cosmo_cpp.bin");
+        DataLoader loader = DataLoader("C:/Local Projects/GradCraft/data/datasets/cosmo_cpp.bin");
         
         // MODEL
         float base_std = 0.02f;
@@ -57,11 +57,11 @@ int main() {
         bool load_checkpoint = false;
         int64_t checkpoint_every = 500;
 
-        std::string latest_model_path = "C:/Local Projects/autograd_cpp/models/mallmoc-180/latest_model.bin";
-        std::string latest_optim_path = "C:/Local Projects/autograd_cpp/models/mallmoc-180/latest_optim.bin";
-        std::string latest_scheduler_path = "C:/Local Projects/autograd_cpp/models/mallmoc-180/latest_scheduler.bin";
+        std::string latest_model_path = "C:/Local Projects/GradCraft/models/mallmoc-180/latest_model.bin";
+        std::string latest_optim_path = "C:/Local Projects/GradCraft/models/mallmoc-180/latest_optim.bin";
+        std::string latest_scheduler_path = "C:/Local Projects/GradCraft/models/mallmoc-180/latest_scheduler.bin";
 
-        std::string final_save_path = "C:/Local Projects/autograd_cpp/models/mallmoc-180/trained_model.bin";
+        std::string final_save_path = "C:/Local Projects/GradCraft/models/mallmoc-180/trained_model.bin";
 
         int64_t start_step = 0;
         if (load_checkpoint == true) {
@@ -83,7 +83,7 @@ int main() {
         // LOG
         int64_t print_every = 10;
         int64_t tokens_per_interval = print_every * grad_accum_steps * B_real * seq_len;
-        std::string loss_log_path = "C:/Local Projects/autograd_cpp/models/mallmoc-180/training_log.csv";
+        std::string loss_log_path = "C:/Local Projects/GradCraft/models/mallmoc-180/training_log.csv";
         bool log_exists = std::filesystem::exists(loss_log_path);
         std::ofstream log_file(loss_log_path, std::ios::app);
         if (!log_file) {
